04ec8d4c8bcac5e40019f494ce72fd486b7097e3
IInmy <haoxujie1993@163.com>
2017-04-09 20:22:08 +0800

    完成“我的”部分的页面内容的展示

--- app/src/main/java/com/jaxsen/xianghacaipu/ui/mine/fragment/MineFragment.java
+++ app/src/main/java/com/jaxsen/xianghacaipu/ui/mine/fragment/MineFragment.java
@@ -3,9 +3,6 @@ package com.jaxsen.xianghacaipu.ui.mine.fragment;
 import com.jaxsen.xianghacaipu.R;
 import com.rock.mvplibrary.base.BaseFragment;
 
-/**
- * Created by Administrator on 2017/4/9.
- */
 
 public class MineFragment extends BaseFragment {
     public static final String TAG = MineFragment.class.getName();
